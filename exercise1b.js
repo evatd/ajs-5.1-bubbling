3db4f3af138c2c20804623744bb377dad8cf515b
Eva <eva.tkavc@gmail.com>
2017-11-20 20:18:54 +0000

    prettier code

--- exercise1b.js
+++ exercise1b.js
@@ -2,19 +2,16 @@
 
 // The elements to attach "click" handlers to
 var docBody = document.body,
-    parentElement = document.getElementById('parent'),
-    childElement = document.getElementById('child');
-
+  parentElement = document.getElementById("parent"),
+  childElement = document.getElementById("child");
 
 // Add your listeners here...
-childElement.addEventListener('mouseleave', function () {
-    // Double quote alert - use escape mechanism \
-    console.log('I\'m the child!'); // Move your mouse out of child so over parent, see "I'm the child!"
-    event.stopPropagation(); // No bubbling
+childElement.addEventListener("mouseleave", function() {
+  // Double quote alert - use escape mechanism \
+  console.log("I'm the child!"); // Move your mouse out of child so over parent, see "I'm the child!"
+  event.stopPropagation(); // No bubbling
 });
 
-parentElement.addEventListener('mouseleave', function () {
-    console.log('I\'m the parent!');  // Move your mouse out of parent so over parent see "I'm the parent!"
+parentElement.addEventListener("mouseleave", function() {
+  console.log("I'm the parent!"); // Move your mouse out of parent so over parent see "I'm the parent!"
 });
-
-
